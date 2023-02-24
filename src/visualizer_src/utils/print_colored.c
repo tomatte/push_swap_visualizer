7ed761f89a155530111851131c5588c329ac1756
tomatte <daniel.brandao2000@gmail.com>
2023-02-24 11:09:39 -0300

    change print_colored() to accept color as argument

--- src/visualizer_src/utils/print_colored.c
+++ src/visualizer_src/utils/print_colored.c
@@ -6,21 +6,21 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 10:04:21 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/02/24 10:04:32 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/24 10:44:00 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header.h"
 
-void	print_colored(t_lst *node)
+void	print_colored(t_lst *node, int color)
 {
 		int		box_start;
-		char	full_box[] = "            ";
-	
 
-		attron(COLOR_PAIR(1));
+		attron(COLOR_PAIR(color));
 		box_start = node->x - (BOXSIZE >> 1) + (num_len(node->num) >> 1);
-		mvprintw(node->y, box_start, "%s", full_box);
+		if (box_start < 0)
+			box_start = 0;
+		mvprintw(node->y, box_start, "%s", FULL_BOX);
 		mvprintw(node->y, node->x, "%d", node->num);
 		attroff(COLOR_PAIR(1));
 }
