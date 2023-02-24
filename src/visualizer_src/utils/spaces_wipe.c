3895123f1f72322b557f8eee0f26eba35c351a3e
tomatte <daniel.brandao2000@gmail.com>
2023-02-24 11:08:25 -0300

    change spaces_wipe to rewrite default background

--- src/visualizer_src/utils/spaces_wipe.c
+++ src/visualizer_src/utils/spaces_wipe.c
@@ -6,22 +6,21 @@
 /*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 09:28:04 by dbrandao          #+#    #+#             */
-/*   Updated: 2023/02/22 09:31:09 by dbrandao         ###   ########.fr       */
+/*   Updated: 2023/02/24 10:43:05 by dbrandao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header.h"
 
-void	spaces_wipe(t_lst *stack)
+void	spaces_wipe(t_lst *node)
 {
-	int	len;
-	int	i;
+	int		box_start;
+	char	full_box[] = "            ";
 
-	len = num_len(stack->num);
-	i = 0;
-	while (i < len)
-	{
-		mvprintw(stack->y, stack->x + i, " ");
-		i++;
-	}
+	attron(COLOR_PAIR(0));
+	box_start = node->x - (BOXSIZE >> 1) + (num_len(node->num) >> 1);
+	if (box_start < 0)
+		box_start = 0;
+	mvprintw(node->y, box_start, "%s", full_box);
+	attroff(COLOR_PAIR(0));
 }
